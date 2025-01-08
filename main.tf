7c3fc574c5643ec69d5d77c2314c027e46b88b6c
elitamsut <e.tamsut@gmail.com>
2025-01-08 10:23:05 +0200

    Add main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
-resource "azurerm_virtual_desktop_host_pool" "example" {
+echo 'resource "azurerm_virtual_desktop_host_pool" "example" {
   name                = "example-host-pool"
   location            = "East US"
   resource_group_name = "existing-resource-group"
   friendly_name       = "Example Host Pool"
   type                = "Pooled"
   load_balancer_type  = "BreadthFirst"
-}
+}' > main.tf
+
