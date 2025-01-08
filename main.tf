56c270454f4681855a3f02de51bb383c52e789ec
elitamsut <e.tamsut@gmail.com>
2025-01-08 10:30:30 +0200

    Add main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,15 @@
-echo 'resource "azurerm_virtual_desktop_host_pool" "example" {
-  name                = "example-host-pool"
-  location            = "East US"
-  resource_group_name = "existing-resource-group"
-  friendly_name       = "Example Host Pool"
-  type                = "Pooled"
-  load_balancer_type  = "BreadthFirst"
-}' > main.tf
+- name: Create main.tf (Simulated)
+  run: |
+    if [ "$SIMULATE_TERRAFORM" = "true" ]; then
+      cat <<EOF > main.tf
+      resource "azurerm_virtual_desktop_host_pool" "example" {
+        name                = "example-host-pool"
+        location            = "East US"
+        resource_group_name = "existing-resource-group"
+        friendly_name       = "Example Host Pool"
+        type                = "Pooled"
+        load_balancer_type  = "BreadthFirst"
+      }
+      EOF
+    fi
 
